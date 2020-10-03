2169fb7609261445b78ef500a795bbf9372eed63
powerdigital-dev <johnny1864@yahoo.com>
2020-10-03 12:40:04 -0700

    added linkedin post component

--- src/app/components/linkedin-post/linkedin-post.component.ts
+++ src/app/components/linkedin-post/linkedin-post.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-linkedin-post',
   templateUrl: './linkedin-post.component.html',
-  styleUrls: ['./linkedin-post.component.scss']
+  styleUrls: ['./linkedin-post.component.scss'],
 })
 export class LinkedinPostComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public get linkedinMessage() {
+    const base = 'https://www.linkedin.com/shareArticle?mini=true';
+    const currentSite = `&url=https://johnny1864.github.io/${this.router.url}`;
+
+    return `${base}${currentSite}`;
   }
-
 }
