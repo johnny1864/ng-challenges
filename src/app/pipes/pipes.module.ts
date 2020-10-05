37dd15a850328d2c86fba6e1eab8342b572e9810
powerdigital-dev <johnny1864@yahoo.com>
2020-10-04 20:39:22 -0700

    added search pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -6,6 +6,7 @@ import { PIPES_ROUTES } from './pipes.routes';
 import { CreditCardFormatterPipe } from './credit-card-formatter.pipe';
 import { TruncatePipe } from './truncate.pipe';
 import { FlattenPipe } from './flatten.pipe';
+import { FilterTermPipe } from './filter-term.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FlattenPipe } from './flatten.pipe';
     CreditCardFormatterPipe,
     TruncatePipe,
     FlattenPipe,
+    FilterTermPipe,
   ],
   exports: [PipesDocumentationComponent],
   imports: [CommonModule, RouterModule.forChild(PIPES_ROUTES)],
